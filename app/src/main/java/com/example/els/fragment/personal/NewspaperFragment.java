cef80277065bd6fc12f04f219768105ffeda372b
thanhphat219 <phat01283249260@gmail.com>
2022-04-12 23:04:12 +0700

    update navigation

---
+++ app/src/main/java/com/example/els/fragment/personal/NewspaperFragment.java
@@ -0,0 +1,42 @@
+package com.example.els.fragment.personal;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.els.R;
+import com.example.els.databinding.FragmentNewspaperBinding;
+
+public class NewspaperFragment extends Fragment {
+    private FragmentNewspaperBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentNewspaperBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.personalNewspaperBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_newspaperFragment_to_personalFragment);
+            }
+        });
+    }
+}
