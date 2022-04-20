ff20fb8947a569fec5d54088884659a9df28aa16
JohnyHo-art <71172797+JohnyHo-art@users.noreply.github.com>
2022-04-20 13:56:11 +0700

    update log out function

--- app/src/main/java/com/example/els/view/PersonalFragment.java
+++ app/src/main/java/com/example/els/view/PersonalFragment.java
@@ -11,20 +11,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.els.R;
 import com.example.els.databinding.FragmentPersonalBinding;
 
 public class PersonalFragment extends Fragment {
-    FragmentPersonalBinding binding;
-    public PersonalFragment() {
-
-    }
-
-
-    public static PersonalFragment newInstance(String param1, String param2) {
-        PersonalFragment fragment = new PersonalFragment();
-        return fragment;
-    }
-
+    private FragmentPersonalBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,7 +23,7 @@ public class PersonalFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //return inflater.inflate(R.layout.fragment_personal, container, false);
 
@@ -59,32 +50,10 @@ public class PersonalFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.personalTabSetting1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(PersonalFragmentDirections.actionPersonalFragmentToInformationFragment());
-            }
-        });
-        binding.personalTabSetting2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(PersonalFragmentDirections.actionPersonalFragmentToAchievementFragment());
-            }
-        });
-
-        binding.personalTabSetting3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(PersonalFragmentDirections.actionPersonalFragmentToNewspaperFragment());
-            }
-        });
-
-        binding.personalSetting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(PersonalFragmentDirections.actionPersonalFragmentToSettingFragment());
-            }
-        });
+        binding.personalTabSetting1.setOnClickListener(view1 -> Navigation.findNavController(view1).navigate(PersonalFragmentDirections.actionPersonalFragmentToInformationFragment()));
+        binding.personalTabSetting2.setOnClickListener(view12 -> Navigation.findNavController(view12).navigate(PersonalFragmentDirections.actionPersonalFragmentToAchievementFragment()));
+        binding.personalTabSetting3.setOnClickListener(view13 -> Navigation.findNavController(view13).navigate(PersonalFragmentDirections.actionPersonalFragmentToNewspaperFragment()));
+        binding.personalSetting.setOnClickListener(view14 -> Navigation.findNavController(view14).navigate(PersonalFragmentDirections.actionPersonalFragmentToSettingFragment()));
+        binding.personalTabSetting4.setOnClickListener(view15 -> Navigation.findNavController(view15).navigate(R.id.action_personalFragment_to_loginFragment));
     }
 }
