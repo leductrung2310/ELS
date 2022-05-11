f3d5573da60a4ef2a56164871dabeb3b6f5bcd1b
JohnyHo-art <71172797+JohnyHo-art@users.noreply.github.com>
2022-05-11 23:30:56 +0700

    Game fragment

--- app/src/main/java/com/example/els/view/games/GamesFragment.java
+++ app/src/main/java/com/example/els/view/games/GamesFragment.java
@@ -1,21 +1,20 @@
-package com.example.els.view.authentication;
+package com.example.els.view.games;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.els.R;
-import com.example.els.databinding.FragmentPhoneLoginBinding;
+import com.example.els.databinding.FragmentGamesBinding;
 
-public class PhoneLoginFragment extends Fragment {
-
-    private FragmentPhoneLoginBinding binding;
+public class GamesFragment extends Fragment {
+    private FragmentGamesBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,13 +24,17 @@ public class PhoneLoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentPhoneLoginBinding.inflate(getLayoutInflater());
+        // Inflate the layout for this fragment
+        binding = FragmentGamesBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.backButtonPhoneLogin.setOnClickListener(view1 -> Navigation.findNavController(view1).navigate(R.id.action_phoneLoginFragment_to_loginFragment));
+
+
     }
+
+
 }
