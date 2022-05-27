1b05b90b1c3da7c18dad701db72755575dd62391
JohnyHo-art <71172797+JohnyHo-art@users.noreply.github.com>
2022-05-27 23:28:04 +0700

    Commit to make sure code is okay

--- app/src/main/java/com/example/els/view/games/WordQuizFragment.java
+++ app/src/main/java/com/example/els/view/games/WordQuizFragment.java
@@ -13,18 +13,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.els.R;
 import com.example.els.databinding.FragmentWordQuizBinding;
 import com.example.els.models.games.Quiz;
+import com.example.els.models.games.QuizGame;
 import com.example.els.viewmodel.games.WordQuizViewModel;
-
 
 public class WordQuizFragment extends Fragment {
     public static final int totalQuiz = 10;
     private FragmentWordQuizBinding binding;
     private WordQuizViewModel quizViewModel;
+    private Button firstBtn;
+    private Button secondBtn;
+    public static int constantsTime = 1500;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +47,8 @@ public class WordQuizFragment extends Fragment {
 
         // Initialize viewmodel
         quizViewModel = new ViewModelProvider(requireActivity()).get(WordQuizViewModel.class);
+        firstBtn = binding.firstAnswerBtn;
+        secondBtn = binding.secondAnswerBtn;
 
         // Fetch quiz list from API when first join in the game
         quizViewModel.fetchQuizFromAPI();
@@ -53,101 +57,141 @@ public class WordQuizFragment extends Fragment {
         quizViewModel.currentQuiz().observe(getViewLifecycleOwner(), currentQuiz -> {
             // Give a delay time for user to see the question and its answer
             (new Handler()).postDelayed(() -> {
-                updateView(currentQuiz);
-            }, 2000);
+                updateQuizViewContent(currentQuiz);
+                updateProgressInfo(currentQuiz);
+            }, constantsTime);
         });
 
-        // Handle event when user choose the first option of answers
-        binding.firstAnswerBtn.setOnClickListener(view1 -> onSubmitAnswer(true));
-
         // Handle event when user choose the second option of answers
-        binding.secondAnswerBtn.setOnClickListener(view12 -> onSubmitAnswer(false));
+        quizViewModel.quizGame().observe(getViewLifecycleOwner(), quizGame -> {
+            // Handle event when user choose the first option of answers
+            firstBtn.setOnClickListener(view1 -> {
+                onFirstOptionChosen(quizGame);
+                if(quizViewModel.currentQuiz().getValue() == totalQuiz) navigateToTheCongratulationsScreen(view1);
+            });
+            // Handle event when user choose the first option of answers
+            secondBtn.setOnClickListener(view12 -> {
+                onSecondOptionChosen(quizGame);
+                if(quizViewModel.currentQuiz().getValue() == totalQuiz) navigateToTheCongratulationsScreen(view12);
+            });
+        });
 
         // handle back event
         binding.backBtn.setOnClickListener(this::onBackBtnPressed);
+
+        // Skip quiz
+        binding.skipThisQuiz.setOnClickListener(view13 -> {
+            quizViewModel.updateQuiz();
+            updateQuizViewContent(quizViewModel.currentQuiz().getValue());
+            updateProgressInfo(quizViewModel.currentQuiz().getValue());
+        });
     }
 
-    // Update view with new quiz, options and score as well as quiz number
-    private void updateView(int currentQuiz) {
-        // Initialize views that need to update
-        Button firstQuestion = binding.firstAnswerBtn;
-        Button secondQuestion = binding.secondAnswerBtn;
-        TextView tvQuiz = binding.quiz;
-        TextView scoreTextView = binding.yourScore;
-        TextView numberQuizTextView = binding.currentQuiz;
-
-        // Observe changes of quiz number and score then update it
-        numberQuizTextView.setText(getString(
-                R.string.current_question,
-                currentQuiz,
-                totalQuiz
-        ));
-        quizViewModel.currentScore().observe(getViewLifecycleOwner(), score -> scoreTextView.setText(getString(
-                R.string.current_score,
-                score,
-                totalQuiz)
-        ));
-
+    // Update quiz content
+    public void updateQuizViewContent(int currentQuiz) {
         // start to observe changes and update it to the views above
         // Use observer to make sure the quizlist changes and non null to avoid crash
         quizViewModel.quizGame().observe(getViewLifecycleOwner(), quizGame -> {
+            // get the current quiz from the quiz list with the given index
             // Current quiz is always greater than the index of quiz 1 unit
             Quiz quiz = quizGame.getQuizList().get(currentQuiz - 1);
             // Set quiz content
-            tvQuiz.setText(getString(R.string.quiz_content,
+            binding.quiz.setText(getString(R.string.quiz_content,
                     quiz.getQuestions().get(0),
                     quiz.getQuestions().get(1),
                     quiz.getQuestions().get(2)
             ));
+            // Make buttons clickable
+            setButtonClickable(true);
             // Set text for answer button text
-            firstQuestion.setText(getString(R.string.first_answer,
+            firstBtn.setText(getString(R.string.first_answer,
                     quiz.getOptions().get(0)
             ));
-            secondQuestion.setText(getString(R.string.second_answer,
+            secondBtn.setText(getString(R.string.second_answer,
                     quiz.getOptions().get(1)
             ));
-            // Update button background back to normal after user submit an answer
-            binding.secondAnswerBtn.setBackgroundColor(getResources().getColor(R.color.accent_color, null));
-            binding.firstAnswerBtn.setBackgroundColor(getResources().getColor(R.color.accent_color, null));
-            // Update progress indicator
-            binding.wordQuizIndicator.setProgress(currentQuiz * 10, true);
+            resetButtonColor();
         });
     }
 
-    // Handle the event after submit answer
-    public void onSubmitAnswer(Boolean isFirstOption) {
-        // Use observer to make sure the quizlist changes and non null to avoid crash
-        quizViewModel.quizGame().observe(getViewLifecycleOwner(), quizGame -> {
-            Quiz quiz = quizGame.getQuizList().get(quizViewModel.currentQuiz().getValue() - 1);
-            // If the user click on the first option then:
-            if (isFirstOption) {
-                if (quiz.getCorrect() == 1) {
-                    // if the correct answer is 1 then change the button color to green and increase the score
-                    binding.firstAnswerBtn.setBackgroundColor(getResources().getColor(R.color.secondary_color_1, null));
-                    quizViewModel.updateScore();
-                } else {
-                    // else set the button color to red color
-                    binding.firstAnswerBtn.setBackgroundColor(getResources().getColor(R.color.secondary_color_3, null));
-                }
-            }
-            // else the the user choose the second option
-            else {
-                if (quiz.getCorrect() == 2) {
-                    // if the correct answer is 2 then change the button color to green and increase the score
-                    binding.secondAnswerBtn.setBackgroundColor(getResources().getColor(R.color.secondary_color_1, null));
-                    quizViewModel.updateScore();
-                } else {
-                    // else set the button color to red color
-                    binding.secondAnswerBtn.setBackgroundColor(getResources().getColor(R.color.secondary_color_3, null));
-                }
-            }
-        });
+    // Update score, quiz index and progress indicator
+    public void updateProgressInfo(int currentQuiz) {
+        // Observe changes of quiz number and score then update it
+        binding.currentQuiz.setText(getString(
+                R.string.current_question,
+                currentQuiz,
+                totalQuiz
+        ));
+        quizViewModel.currentScore().observe(getViewLifecycleOwner(), score -> binding.yourScore.setText(getString(
+                R.string.current_score,
+                score,
+                totalQuiz)
+        ));
+        // Update progress indicator
+        binding.wordQuizIndicator.setProgress(currentQuiz * 10, true);
+    }
 
-        // If this the final quiz then show a congratulation dialog
+    public void onFirstOptionChosen(QuizGame quizGame) {
+        // Set clickable to false to avoid user click so many times
+        setButtonClickable(false);
 
+        Quiz quiz = quizGame.getQuizList().get(quizViewModel.currentQuiz().getValue() - 1);
+        // if the correct answer is 1 then change the button color to green and increase the score
+        if (quiz.getCorrect() == 1) {
+            setButtonStateRight(firstBtn);
+            quizViewModel.updateScore();
+        } else {
+            // else set the button color to red color
+            setButtonStateFalse(firstBtn);
+        }
 
         // Finally update the current quiz
         quizViewModel.updateQuiz();
+    }
+
+    public void onSecondOptionChosen(QuizGame quizGame) {
+        // Set clickable to false to avoid user click so many times
+        setButtonClickable(false);
+
+        Quiz quiz = quizGame.getQuizList().get(quizViewModel.currentQuiz().getValue() - 1);
+        // if the correct answer is 1 then change the button color to red
+        if (quiz.getCorrect() == 1) {
+            setButtonStateFalse(secondBtn);
+        } else {
+            // else set the button color to green color and increase the score
+            setButtonStateRight(secondBtn);
+            quizViewModel.updateScore();
+        }
+
+        // Finally update the current quiz
+        quizViewModel.updateQuiz();
+    }
+
+    // Navigate to the congratulations fragment
+    public void navigateToTheCongratulationsScreen(View view) {
+        (new Handler()).postDelayed(() ->
+                Navigation.findNavController(view).navigate(R.id.action_wordQuizFragment_to_congratulationFragment),
+                constantsTime
+        );
+    }
+
+    // Set state of the buttons
+    public void setButtonStateRight(Button button) {
+        button.setBackgroundColor(getResources().getColor(R.color.secondary_color_1, null));
+    }
+    public void setButtonStateFalse(Button button) {
+        button.setBackgroundColor(getResources().getColor(R.color.secondary_color_3, null));
+    }
+    public void resetButtonColor() {
+        // Update button background back to normal after user submit an answer
+        firstBtn.setBackgroundColor(getResources().getColor(R.color.accent_color, null));
+        secondBtn.setBackgroundColor(getResources().getColor(R.color.accent_color, null));
+    }
+
+    // Set button clickable or not
+    public void setButtonClickable(boolean isClickable) {
+        firstBtn.setClickable(isClickable);
+        secondBtn.setClickable(isClickable);
     }
 
     // Handle on back button click
